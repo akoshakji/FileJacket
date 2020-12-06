f2ddafd56a03a1c70a01fab47ef90654d4d97ca0
Anwar Koshakji <koshakji@mit.edu>
2020-12-06 13:06:15 +0100

    Override __eq__ for Directory and File class

--- Directory.py
+++ Directory.py
@@ -5,3 +5,19 @@ class Directory:
         self.timestamp = timestamp
         self.children = []
         self.files = []
+
+    def __eq__(self, other):
+        if isinstance(other, Directory):
+            return ((self.name == other.name) and 
+                    (self.timestamp == other.timestamp))
+        return False
+
+if __name__ == '__main__':
+    d1 = Directory('d1', 162076)
+    d2 = Directory('d2', 162076)
+    d3 = Directory('d1', 162075)
+    d4 = Directory('d1', 162076)
+    
+    print(d1 == d2)
+    print(d1 == d3)
+    print(d1 == d4)
--- File.py
+++ File.py
@@ -3,3 +3,19 @@ class File:
     def __init__(self, name, timestamp):
         self.name = name
         self.timestamp = timestamp
+    
+    def __eq__(self, other):
+        if isinstance(other, File):
+            return ((self.name == other.name) and 
+                    (self.timestamp == other.timestamp))
+        return False
+
+if __name__ == '__main__':
+    f1 = File('f1', 162076)
+    f2 = File('f2', 162076)
+    f3 = File('f1', 162075)
+    f4 = File('f1', 162076)
+    
+    print(f1 == f2)
+    print(f1 == f3)
+    print(f1 == f4)
