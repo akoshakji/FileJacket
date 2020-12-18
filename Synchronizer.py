f7195822e8eec10d05ccfce78e2a619e3df0a3cc
Anwar Koshakji <anwar.koshakji@gmail.com>
2020-12-18 16:54:46 +0100

    Synchronizer: Added documentation

--- Synchronizer.py
+++ Synchronizer.py
@@ -5,115 +5,211 @@ from Filesystem import Filesystem
 from DropboxManager import DropboxManager
 
 class Synchronizer:
+    '''
+    Class that synchronizes local directories with the remote server.
     
+    The local filesystem of the target directory is compared with the
+    previous state of the filesystem of the same directory, which has
+    been saved in a pickle. This choice aims to limit to the minimum
+    the communication with the remote server, the most time consuming part
+    and connection dependent.
+    
+    Files or directories on the remote that are not present locally are cleaned
+    from the remote.
+    
+    Underlying assumption: The remote filesystem should be a mirror of the local filesystem.
+    Therefore, directories on the remote should not be modified manually.
+    If that happens, the modifications will not be included in the pickled filesystem
+    and this could lead to errors during synchronization.
+    '''
+
     def __init__(self, fs_local):
+        # local filesystem
         self.fs_local = fs_local
-        self.root_base_path = os.path.dirname(fs_local.root.path)
+        # base path 
+        self.base_path = os.path.dirname(fs_local.root.path)
+        # list of paths of files or directories to delete
         self.list_to_delete = []
-        
+
+        #TODO move all dropbox objects and introduce abstraction
+        # access token
         ACCESS_TOKEN = ""
+        # prefix path to the remote root directory
         self.dbx_prefix = '/'
+        # path to remote root directory
         dbx_root_dir = self.dbx_prefix + os.path.basename(fs_local.root.path)
+        # start DropboxManager
         self.dbx = DropboxManager(ACCESS_TOKEN, dbx_root_dir)
-        
+
+        # if the remote root directory does not exist
         if not self.check_directory_exists(dbx_root_dir):
+            # upload it entirely
             self.upload_directory(fs_local.root)
 
 
     def sync(self, fs_pickled, dir1=None, dir2=None):
+        '''
+        Synchronize local filesystem on the remote
+
+        fs_pickled ----> previous state of the filesystem
+        dir1 ----> directory in the local filesystem
+        dir2 ----> directory in the pickled filesystem
+        '''
         print("Checking filesystem..")
+        # if dir1 and dir2 are not specified
         if (dir1 is None) and (dir2 is None):
+            # check that the remote root directory is the same
             assert self.fs_local.root.name == fs_pickled.root.name # TODO: what if renamed?
+            # then initialize
             dir1 = self.fs_local.root
             dir2 = fs_pickled.root
 
+        # loop over all local files 
         for entry_file in dir1.files:
+            # if the file is present in dir2
             if entry_file in dir2.files:
                 print("Files are the same", entry_file.name)
                 continue
-            print("File Not Found, uploading it..", entry_file.name)
+            # if the file is not present in dir2, upload it
+            print("Uploading File..", entry_file.name)
             # upload the file
             self.upload_file(entry_file)
 
+        # loop over all local subdirectories
         for entry_dir in dir1.children:
+            # if the path of subdirectory exists in dir2
             if any(entry_dir.path == x.path for x in dir2.children):
                 print("Dir found, enter and check subfolders files..", entry_dir.name)
                 #dir1 = entry_dir
-                
+
+                # retrieve the index of the subdirectory
                 index = [i for i,x in enumerate(dir2.children) if x.path==entry_dir.path][0]
                 #dir2 = dir2.children[index]
                 
+                # synchronize the subdirectory
                 self.sync(fs_pickled, entry_dir, dir2.children[index])
                 #self.sync(fs_pickled, dir1, dir2)
             else:
+                # the directory is not on the remote
                 print("Dir Not Found, create it, and upload all of its contents...", entry_dir.name)
+                # upload the directory
                 self.upload_directory(entry_dir)
 
 
     def upload_file(self, file_item):
+        '''
+        Upload local file
+        '''
+        # path on the remote
         dbx_item_path = self.get_remote_path(file_item.path)
+        # upload the file
         self.dbx.upload_file(file_item, dbx_item_path)
         print("File", file_item.name, "Uploaded!")
 
 
     def upload_directory(self, directory_item):
+        '''
+        Upload local directory
+        '''
         print("Creating directory on dropbox", directory_item.name, "..")
+        # path on the remote
         dbx_item_path = self.get_remote_path(directory_item.path)
+        # create the directory on the remote
         self.dbx.create_directory(dbx_item_path)
-        
+
+        # upload all files
         for entry_file in directory_item.files:
             print("Uploading", entry_file.name, "..")
             self.upload_file(entry_file)
 
+        # upload all subdirectories
         for entry_dir in directory_item.children:
             print("Creating subdirectory..")
             self.upload_directory(entry_dir)
     
     
     def check_directory_exists(self, dbx_item_path):
+        '''
+        Check that the directory exists on the remote
+        '''
         return self.dbx.check_directory_exists(dbx_item_path)
     
     
     def clean(self, fs_pickled):
+        '''
+        Clean the files on the remote that are not present locally
+        '''
+        # fill the list of files or directories to delete
         print("Filling delete list..")
         self.fill_delete_list(fs_pickled)
         print("List of files to delete: ", self.list_to_delete)
+
+        # if the list is not null
         if self.list_to_delete:
+            # get the corresponding path on the remote
             self.list_to_delete = [self.get_remote_path(x) for x in self.list_to_delete]
+            # then clean
             self.dbx.clean(self.list_to_delete)
         else:
             print("Nothing to clean!")
     
     
     def get_remote_path(self, item_path):
-        return self.dbx_prefix + os.path.relpath(item_path, self.root_base_path)
+        '''
+        Get the path on the remote given the local path
+        '''
+        return self.dbx_prefix + os.path.relpath(item_path, self.base_path)
 
 
     def fill_delete_list(self, fs_pickled, dir1=None, dir2=None):
+        '''
+        Fill the list of files and directories to delete
+
+        fs_pickled ----> previous state of the filesystem
+        dir1 ----> directory in the local filesystem
+        dir2 ----> directory in the pickled filesystem
+        '''
+        # if dir1 and dir2 are not specified
         if (dir1 is None) and (dir2 is None):
+            # initialize
             dir1 = self.fs_local.root
             dir2 = fs_pickled.root
 
+        # loop over the subdirectories of dir2
         for entry_dir in dir2.children:
+            # if the path of the subdirectory does not exist in dir1 subdirectories
             if not any(entry_dir.path == x.path for x in dir1.children):
+                # add the path to the list to delete
                 self.list_to_delete.append(entry_dir.path)
             else:
+                # otherwise, retrieve the index of the subdirectory
                 index = [i for i,x in enumerate(dir1.children) if x.path==entry_dir.path][0]
+                # check recursively if the subdirectory contains items to delete
                 self.fill_delete_list(fs_pickled, dir1.children[index], entry_dir)
-        
+
+        # loop over the files in dir2
         for entry_file in dir2.files:
+            # if the file is not present
             if not any(entry_file.path == x.path for x in dir1.files):
+                # add the path of the file to the list
                 self.list_to_delete.append(entry_file.path)
 
 
+#TODO these methods do not belong to the Synchronizer
 def dump_pickle(filesystem, pickle_file_name):
+    '''
+    Dump pickle of Python object
+    '''
     print("Dumping pickle..")
-    with open(pickle_file_name, 'wb') as file: # pickle file name should be the same as principal dir
+    with open(pickle_file_name, 'wb') as file:
         # Pickle the 'data' dictionary using the highest protocol available.
         pickle.dump(filesystem, file, pickle.HIGHEST_PROTOCOL)
 
 
 def load_pickle(pickle_file_name):
+    '''
+    Unpickle Python object
+    '''
     with open(pickle_file_name, 'rb') as file:
         print("Unpickling..")
         # The protocol version used is detected automatically, so we do not
