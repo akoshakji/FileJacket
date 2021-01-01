5b9c9a0d7bf61bc94183c674b0b2458d362b79c7
Anwar Koshakji <anwar.koshakji@gmail.com>
2021-01-01 13:43:30 +0100

    Directory class is now iterable

--- Directory.py
+++ Directory.py
@@ -45,7 +45,23 @@ class Directory:
                 + path + "\n\t" \
                 + files + "\n\t" \
                 + children
+    
+
+    def __iter__(self):
+        yield self
+        for child in self.children:
+            for item in child:
+                yield item
 
 
 if __name__ == '__main__':
-    pass
+    d1 = Directory('test', 1234, '/Users/anwar/test')
+    d2 = Directory('test1', 12345, '/Users/anwar/test/test1')
+    d3 = Directory('test2', 12346, '/Users/anwar/test/test2')
+    d4 = Directory('test3', 12347, '/Users/anwar/test/test1/test3')
+    d1.children.append(d2)
+    d1.children.append(d3)
+    d1.children[-1].children.append(d4)
+
+    for item in d1:
+        print(item)
